79cc2a6b9236b59920365b7ce76a31a0eee7736f
momerio <84517709+momerio@users.noreply.github.com>
2021-08-20 12:18:01 +0900

    Update xml_run_.py
    サンプルコード

--- テンプレート/xml_run_.py
+++ テンプレート/xml_run_.py
@@ -1,35 +1,15 @@
 import convXML as MakeLine
-import xml.etree.ElementTree as ET
-from xml.dom import minidom
-import queue
+
 
 
 # 出力ファイルパス
-OUTPUT_FILE_PATH = "./volume_test_a.xml"
-
-
-def ToMinidom(elem):
-    rough_string = ET.tostring(elem, 'utf-8')
-    return minidom.parseString(rough_string)
-
-
-def Fairing(rootN):
-    R = ToMinidom(rootN).toprettyxml(indent="\t", newl="\n", encoding="UTF-8")
-    # R=R.replaceex("?>","?>"+XSL,1)
-    return R
-
-
-ret = []
-
-
-def make(name, line, command={}):
-    global ret
-    ret.append(MakeLine.Make(name, line, command))
+OUTPUT_FILE_PATH = "./volume_test.xml"
 
 
 # 初期化
-MakeLine.Reset()
-ret.append(MakeLine.Refresh())
+MakeLine.init()
+make = MakeLine.make
+
 
 ##########################################
 #               漫才台本部分
@@ -41,6 +21,7 @@ ret.append(MakeLine.Refresh())
 line: 発話部分(合成音声の仕様により読み間違える場合はひらがな指定をする)
 "ballon": 表示部分
 """
+
 make("mary", {
     "line": "どーも、あいちゃんです",
     "balloon": "どーも、あいちゃんです"
@@ -115,18 +96,7 @@ make("bob", {
     }}
 )
 
-############################
-#       台本生成部分
-############################
-rootN = ET.Element("manzai")
-metaD = ET.SubElement(rootN, "meta")
-metaD.set("content", "HeadlineNews")
-script = ET.SubElement(rootN, "script")
-Q = queue.Queue()
-# PlaceManzai.make(logger,Q,Pref,City)
-[Q.put(E) for E in ret]
-while not Q.empty():
-    script.append(Q.get())
-R = Fairing(rootN)
-with open(OUTPUT_FILE_PATH, "wb" if isinstance(R, bytes) else "w") as f:
-    f.write(R)
+##########################################
+#               漫才台本出力
+##########################################
+MakeLine.makeScript(OUTPUT_FILE_PATH)
